c541196e443df1cab53ddf99bf74a2763c7378c5
vladimirtischler <tischler.vladimir@gmail.com>
2019-11-07 20:29:04 +0100

    sdc

---
+++ Open-Lab-02.9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Open_Lab_02._9
+{
+    class Program
+    {
+        public static string ConcatName(string name1, string name2)
+        {
+            return ($"{name1}, {name2}");
+        }
+        static void Main(string[] args)
+        {
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+            Console.WriteLine(ConcatName(str1, str2));
+        }
+    }
+}
